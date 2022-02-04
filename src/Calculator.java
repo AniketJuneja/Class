1700bc5dbb9b0bac14379c5743f696c633f7bf08
Lenovo <aniketjuneja2000@gmail.com>
2022-02-04 15:41:03 +0530

    Commit

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    public void add(int a, int b) {
+        int total = a + b;
+        System.out.println(total);
+    }
+
+    public void subtract(int a, int b) {
+        int total = a - b;
+        System.out.println(total);
+    }
+
+    public void multiply(int a, int b) {
+        int total = a * b;
+        System.out.println(total);
+    }
+
+    public void divide(int a, int b) {
+        int total = a / b;
+        System.out.println(total);
+    }
+}
+
+
+
+
+
+
+
